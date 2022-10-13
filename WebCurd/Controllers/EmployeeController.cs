74de7c80ed016a1b99df9d575238d657ff210b76
Thirumr <rajesh.thirumurthylu@equiniti.com>
2022-10-13 12:34:36 +0000

    web part and api part

---
+++ WebCurd/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using WebCrud.Models;
+
+namespace WebCrud.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IConfiguration _config;
+        public EmployeeController(IConfiguration config)
+        {
+            _config=config; 
+        }
+        public IActionResult Index()
+        {
+            List<EmployeeWebModel> employeeWebModels = new List<EmployeeWebModel>();
+            HttpClient client = new HttpClient();   
+            client.BaseAddress= new Uri(_config.GetValue<string>("BaseApiUrl")+"Get");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = client.GetAsync(_config.GetValue<string>("BaseApiUrl") + "Get").Result;
+            employeeWebModels=JsonConvert.DeserializeObject<List<EmployeeWebModel>>(response.Content.ReadAsStringAsync().Result);
+            return View(employeeWebModels);
+        }
+    }
+}
---
+++ WebCurd/Models/EmployeeWebModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebCrud.Models
+{
+    public class EmployeeWebModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Role { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
