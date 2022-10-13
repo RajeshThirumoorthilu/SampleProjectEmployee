1d4f8c0ecc904ee6858ad743bae17a96ac607f6c
Thirumr <rajesh.thirumurthylu@equiniti.com>
2022-10-13 13:42:26 +0000

    changes for add

--- WebCurd/Controllers/EmployeeController.cs
+++ WebCurd/Controllers/EmployeeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Net.Http.Headers;
+using System.Text;
 using WebCrud.Models;
 
 namespace WebCrud.Controllers
@@ -21,6 +22,22 @@ namespace WebCrud.Controllers
             var response = client.GetAsync(_config.GetValue<string>("BaseApiUrl") + "Get").Result;
             employeeWebModels=JsonConvert.DeserializeObject<List<EmployeeWebModel>>(response.Content.ReadAsStringAsync().Result);
             return View(employeeWebModels);
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        public IActionResult PushEmployee(EmployeeWebModel employeeWebModel)
+        {
+            StringContent requestContent;
+            var modifiedAssetJSON = JsonConvert.SerializeObject(employeeWebModel);
+            requestContent = new StringContent(modifiedAssetJSON, Encoding.UTF8, "application/json");
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(_config.GetValue<string>("BaseApiUrl") + "Insert");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = client.PostAsync(_config.GetValue<string>("BaseApiUrl") + "Insert", requestContent).Result;
+            var result =response.Content.ReadAsStringAsync().Result;
+            return Json("Success");
         }
     }
 }
